824b6cb859b2caa8c79a7a934f37c2918681ab15
jose24529 <jose24529@cbtis75.edu.mx>
2026-05-05 18:16:13 -0600

    Arreglando error de mongoose

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // --- 2. CONEXIÓN A MONGODB ---
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Conectado a MongoDB'))
     .catch(err => console.error('Error al conectar:', err));
 
